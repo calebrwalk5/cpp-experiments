a60c495c43dd1472d0225673502f748b6cecd6a5
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-04-20 12:28:47 -0400

    Update loops.cpp

--- loops.cpp
+++ loops.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 int main() {
+  // For Loop
   for (i = 0; i < 2; i++) {
     cout << "for " << i << "\n";
   }
   cout << "for loop complete\n";
+  // While Loop
   int x = 0;
   while (x < 2) {
     cout << "while " << x << "\n";
